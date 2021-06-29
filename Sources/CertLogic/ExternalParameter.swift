9952cc747640adbde76695ab987f5b9d86b40fc5
Alexandr Chernyy <pingus.nikalex@gmail.com>
2021-06-29 16:41:03 +0300

    added issuerCountryCode

--- Sources/CertLogic/ExternalParameter.swift
+++ Sources/CertLogic/ExternalParameter.swift
@@ -14,13 +14,14 @@ public class ExternalParameter: Codable {
   public var validationClock: Date
   public var valueSets: Dictionary<String, [String]>
   public var countryCode: String
+  public var issueCountryCode: String
   public var exp: Date
   public var iat: Date
   public var certificationType: CertificateType = .general
   public var kid: String?
 
   enum CodingKeys: String, CodingKey {
-    case validationClock, valueSets, countryCode, exp, iat
+    case validationClock, valueSets, countryCode, exp, iat, issueCountryCode
   }
   
   public init(validationClock: Date,
@@ -28,13 +29,15 @@ public class ExternalParameter: Codable {
        countryCode: String,
        exp: Date,
        iat: Date,
-       certificationType: CertificateType) {
+       certificationType: CertificateType,
+       issueCountryCode: String) {
     self.validationClock = validationClock
     self.valueSets = valueSets
     self.countryCode = countryCode
     self.exp = exp
     self.iat = iat
     self.certificationType = certificationType
+    self.issueCountryCode = issueCountryCode
   }
   
   required public init(from decoder: Decoder) throws {
@@ -44,5 +47,6 @@ public class ExternalParameter: Codable {
     countryCode = try container.decode(String.self, forKey: .countryCode)
     exp = try container.decode(Date.self, forKey: .exp)
     iat = try container.decode(Date.self, forKey: .iat)
+    issueCountryCode = try container.decode(String.self, forKey: .issueCountryCode)
   }
 }
