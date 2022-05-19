448d9d4d1237cfe3cccfc05483ba12f339df883e
Igor Khomiak <ihorKhomyak@gmail.com>
2022-05-19 09:48:18 +0300

    Feature/fixed date formatter (#54)
    * updated version
    
    * fixed branch
    
    * increased version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "JsonLogic", url: "https://github.com/eu-digital-green-certificates/json-logic-swift.git", from: "1.2.2"),
+        .package(name: "JsonLogic", url: "https://github.com/eu-digital-green-certificates/json-logic-swift.git", from: "1.2.3"),
         .package(name: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0")
     ],
     targets: [
